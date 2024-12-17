de1403cda59488f292163d5061d245d9b770bb15
Francisco Serrano <fserrano@apple.com>
2024-12-16 19:52:55 -0800

    Initial Commit

---
+++ Quartet/QuartetApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuartetApp.swift
+//  Quartet
+//
+//  Created by Francisco Serrano on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct QuartetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
